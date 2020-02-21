1270567641d0e5fb13975a208292094a3bf07588
Rayyan Merchant <rayyanmerchant7@gmail.com>
2020-02-21 15:51:38 +0530

    First Commit

---
+++ appy/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import PostViewSet
+
+router = routers.DefaultRouter()
+router.register('api/post', PostViewSet, 'post')
+
+urlpatterns = router.urls
